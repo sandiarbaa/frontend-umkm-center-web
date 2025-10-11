612c55aec2191bd76762dff2d4173d32357ebdb4
sandiarbaa <sandiarbaa@gmail.com>
2025-10-11 21:49:19 +0700

    fix error same

--- src/app/(admin)/(main-page)/event/page.tsx
+++ src/app/(admin)/(main-page)/event/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import EventTable from "@/components/tables/EventTable";
 import Alert from "@/components/ui/alert/Alert";
 import Button from "@/components/ui/button/Button";
