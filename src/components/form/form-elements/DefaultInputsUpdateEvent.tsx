485e2fd95b366565ab3af09e583e91dda615394e
sandiarbaa <sandiarbaa@gmail.com>
2025-10-11 21:45:13 +0700

    fix error search param in event page

--- src/components/form/form-elements/DefaultInputsUpdateEvent.tsx
+++ src/components/form/form-elements/DefaultInputsUpdateEvent.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import React, { useEffect, useState } from "react";
 import ComponentCard from "../../common/ComponentCard";
 import Label from "../Label";
